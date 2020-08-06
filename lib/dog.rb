749eefe9957190f3ca9518fe883eb9a5dc735a0a
KRStoddard <kimberlynrstoddard@gmail.com>
2020-08-06 12:10:51 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
-  # code goes here
-end
+  
+  #initialize with changeable owner and mood and static name
+  attr_reader :name 
+  attr_accessor :owner, :mood
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    @mood = "nervous"
+
+    Dog.all << self 
+  end 
+
+  #allow access to all instances of Dog class
+  def self.all 
+    @@all 
+  end 
+
+end #of Dog class
