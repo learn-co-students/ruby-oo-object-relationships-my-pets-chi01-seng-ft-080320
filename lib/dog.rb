7189f0c623f829d8ec842a986f3fe774cb150bff
mizzygonzo <xeligon@gmail.com>
2020-08-08 21:48:19 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name 
+
+  @@all = []
+
+  def self.all 
+    @@all
+  end 
+
+  def initialize(name,owner)
+    @name = name 
+    @mood = "nervous"
+    @owner = owner 
+    
+    @@all << self 
+  end 
 end
